2dd86a8a2101b3fd014287506bc686d0e7e2b9fa
Ar0x87 <roks88869@gmail.com>
2022-05-25 13:18:11 +0300

    task40

--- src/units/unit2/exercisesTable/Exercise40.java
+++ src/units/unit2/exercisesTable/Exercise40.java
@@ -1,5 +1,26 @@
 package units.unit2.exercisesTable;
-
+//reference to 7.6
 public class Exercise40 implements Task{
 
+    protected int first = (int) (Math.random() * 1000);
+    protected int second = (int) (Math.random() * 1000);
+    String res;
+
+    private String WhichOne(int x, int y) {
+        if (x == y) {
+            res = "Numbers are equal";
+        } else {
+            if (x > y) {
+                res = "First number is bigger";
+            }
+            if (x < y) {
+                res = "First number is smaller";
+            }
+        }
+        return res;
+    }
+
+    public void writer() {
+        System.out.println(WhichOne(first, second));
+    }
 }
